b46b2d72a19bcaec7d28e4bdd9340d334f629f1b
Mariya Kartseva <kjojoj@rambler.ru>
2021-11-30 00:19:42 +0300

    feat(ht.js): new feat

--- ht.js
+++ ht.js
@@ -1,11 +1,37 @@
-let num = 26621;
+const lang = prompt("Выберите язык (ru/en)");
 
-let arr = Array.from(num.toString(), Number); // распилили числона массив цифр
+const week = {
+    ru: "Пн, Вт, Ср, Чт, Пт, Сб, Вс",
+    en: "Mon, Tue, Wed, Thu, Fri, Sat, Sun",
+};
 
-let result = arr.reduce((acc, mult) => acc * mult); // проход по массиву с вычислением значения
 
-console.log("Результат " + result);
 
-console.log("Возведение в степень " + result**3);
+// ========================= 1 задача
+console.log("1 способ");
+if (lang === "ru")
+    console.log("Пн, Вт, Ср, Чт, Пт, Сб, Вс");
+else
+    console.log("Mon, Tue, Wed, Thu, Fri, Sat, Sun");
 
-console.log("Первые две цифры результата " + String(result**3).slice(0, 2)); // перевели в строку, вычленили подстроку из нужного количества символов
+console.log("2 способ");
+
+switch (lang) {
+    case "ru":
+        console.log("Пн, Вт, Ср, Чт, Пт, Сб, Вс");
+        break;
+    case "en":
+        console.log("Mon, Tue, Wed, Thu, Fri, Sat, Sun");
+        break;
+    default:
+        console.log("что-то пошло не так");
+        break;
+}
+
+console.log("3 способ");
+console.log(week[lang]);
+
+// ========================= 2 задача
+
+const namePerson = prompt("Напишите имя");
+namePerson == "Артем" ? console.log("директор") : (namePerson == "Александр" ? console.log("преподаватель") : console.log("студент"));
