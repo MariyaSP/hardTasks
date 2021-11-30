de45eeb66b3a8f61ea3ad07a6ce652d04a265d46
Mariya Kartseva <kjojoj@rambler.ru>
2021-11-30 22:56:28 +0300

    fix(ht.js): all fix

--- ht.js
+++ ht.js
@@ -1,37 +1,26 @@
+"use strict"
 const lang = prompt("Выберите язык (ru/en)");
 
-const week = {
-    ru: "Пн, Вт, Ср, Чт, Пт, Сб, Вс",
-    en: "Mon, Tue, Wed, Thu, Fri, Sat, Sun",
-};
+const week = new Map([['ru', 'Пн, Вт, Ср, Чт, Пт, Сб, Вс'],
+['en', 'Mon, Tue, Wed, Thu, Fri, Sat, Sun']]);
+console.log(week.get(lang));
 
-
-
-// ========================= 1 задача
-console.log("1 способ");
 if (lang === "ru")
-    console.log("Пн, Вт, Ср, Чт, Пт, Сб, Вс");
+    console.log(week.get('ru'));
 else
-    console.log("Mon, Tue, Wed, Thu, Fri, Sat, Sun");
-
-console.log("2 способ");
+    console.log(week.get('en'));
 
 switch (lang) {
     case "ru":
-        console.log("Пн, Вт, Ср, Чт, Пт, Сб, Вс");
+        console.log(week.get('ru'));
         break;
     case "en":
-        console.log("Mon, Tue, Wed, Thu, Fri, Sat, Sun");
+        console.log(week.get('en'));
         break;
     default:
         console.log("что-то пошло не так");
         break;
 }
-
-console.log("3 способ");
-console.log(week[lang]);
-
-// ========================= 2 задача
 
 const namePerson = prompt("Напишите имя");
 namePerson == "Артем" ? console.log("директор") : (namePerson == "Александр" ? console.log("преподаватель") : console.log("студент"));
