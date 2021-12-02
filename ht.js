15f83ae986a12b49a6b685f993fc121b2ba4db15
Mariya Kartseva <kjojoj@rambler.ru>
2021-12-02 14:45:24 +0300

    feat(ht.js): new feat

--- ht.js
+++ ht.js
@@ -1,16 +1,22 @@
 'use strict';
-const myText = "     hgjhg 666 666 979 jhgjfhgjhsdf iuyiuy yeiyiry           ";
+let arr = [];
 
-function getEditStr(myString) {
+for (let i = 0; i < 7; i++) {
+    arr[i] = prompt("Введите " + (i + 1) + " элемент массива");
 
-    if (typeof myString !== "string") { return "Не строка"; }
-    else {
-        myString = myString.trim();
-        if (myString.length > 30) {
-            myString = myString.split(0, 31) + "...";
-        }
-        return myString;
-    }
+    if (arr[i].slice(0, 1) === "2" || arr[i].slice(0, 1) === "4")
+        console.log(arr[i]);
 }
+console.log(arr);
 
-console.log('Результат ', getEditStr(myText));
+console.log("Простые числа");
+for (let i = 2; i <= 100; i++) {
+    let flag = true;
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0)
+            flag = false;
+    }
+
+    if (flag)
+        console.log(i);
+}
