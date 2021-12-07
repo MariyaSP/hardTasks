37e79f11af4101419230ef4445c3b5a6075c37ad
MariyaSP <95083881+MariyaSP@users.noreply.github.com>
2021-12-07 11:07:01 +0300

    fix(script.js): all fixed

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-const weekList = document.querySelector('ul');
+const div = document.createElement('div');
+const weekList = document.createElement('ul');
+
 const weeks = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
 let listDay;
+let day='';
 const date = new Date();
 
 for (let key in weeks) {
     listDay = document.createElement('li');
+    day=weeks[key];
     if (key >= 5)
-        listDay.classList = "weekend";
+        day=day.italics();
     if (key == (date.getDay() - 1))
-        listDay.classList.add('today');
+        day=day.bold();
 
-    listDay.innerHTML = weeks[key];
+    listDay.innerHTML = day;
     weekList.append(listDay);
 }
 
+div.append(weekList);
+document.body.append(div)
